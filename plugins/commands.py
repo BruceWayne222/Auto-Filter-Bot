787e32f8cfb91d7f82dd2015e44377752f0b43d3
BruceWayne222 <77908276+BruceWayne222@users.noreply.github.com>
2021-12-17 18:45:29 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -4,7 +4,6 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 from utils import Media, get_file_details
-from info import TUTORIAL 
 from pyrogram.errors import UserNotParticipant
 logger = logging.getLogger(__name__)
 
